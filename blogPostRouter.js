56fe40d7591c61e84527097d1c609c8411b40990
0peron <phubbard89@gmail.com>
2017-01-23 18:48:50 -0500

    updated app to use mongoose

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
+const mongoose = require('mongoose');
+mongoos.Promise = global.Promise;
+const {
+    PORT, DATABASE_URL
+} = require('./config');
+
 const {
     BlogPosts
 } = require('./models');
@@ -15,59 +21,142 @@ BlogPosts.create('peppers', 'this is a blog post about them spicy peppers!', 'ho
 
 
 router.get('/', (req, res) => {
-    res.json(BlogPosts.get());
+    //    res.json(BlogPosts.get());
+    BlogPosts
+        .find()
+        .limit(10)
+        .exe()
+        .then(BlogPosts => {
+            res.json({
+                BlogPosts: BlogPosts.map(
+                    (BlogPosts) => BlogPosts.apiRepr())
+            });
+        })
+        .catch(
+            err => {
+                console.error(err);
+                res.status(500).json({
+                    message: 'Internal server Error'
+                });
+            });
 });
 
+router.get('/BlogPosts/:id', (req, res) => {
+    BlogPosts
+        .findById(req.params.id)
+        .exec()
+        .then(BlogPosts => res.json(BlogPosts.apiRepr()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                message: 'Internal server error'
+            })
+        });
+});
 
-router.post('/', jsonParser, (req, res) => {
+router.post('/BlogPosts', jsonParser, (req, res) => {
+    //    const requiredFields = ['title', 'content', 'author'];
+    //    for (let i = 0; i < requiredFields.length; i++) {
+    //        const field = requiredFields[i];
+    //        if (!(field in req.body)) {
+    //            const message = `Missing \`${field}\` in request body`
+    //            console.error(message);
+    //            return res.status(400).send(message);
+    //        }
+    //    }
+    //    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    //    res.status(201).json(item);
     const requiredFields = ['title', 'content', 'author'];
-    for (let i = 0; i < requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing \`${field}\` in request body`
-            console.error(message);
-            return res.status(400).send(message);
+    requiredFields.forEach(field => {
+        if (!(field in req.body && req.body[field])) {
+            return res.status(400).json({
+                message: `Must specify value for ${field}`
+            });
         }
-    }
-    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-    res.status(201).json(item);
+    });
+    BlogPosts
+        .create({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author,
+            publishDate: req.body.publishDate
+        })
+        .then(
+            BlogPosts => res.status(201).json(BlogPosts.apiRepr()))
+        .catch(err => {
+            res.status(500).json({
+                message: 'Internal server Error'
+            });
+        });
 });
 
 
 router.delete('/:id', (req, res) => {
-    BlogPostss.delete(req.params.id);
-    console.log(`Deleted blog item \`${req.params.ID}\``);
-    res.status(204).end();
+    //    BlogPostss.delete(req.params.id);
+    //    console.log(`Deleted blog item \`${req.params.ID}\``);
+    //    res.status(204).end();
+    BlogPosts
+        .findByIdAndRemove(req.params.id)
+        .exe()
+        .then(BlogPosts => res.status(204).end())
+        .catch(err => res.status(500).json({
+            message: 'internal server error'
+        }));
 });
 
 
 router.put('/:id', jsonParser, (req, res) => {
-    const requiredFields = ['title', 'content', 'author', 'publishDate', 'id'];
-    for (let i = 0; i < requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing \`${field}\` in request body`
-            console.error(message);
-            return res.status(400).send(message);
-        }
+    //    const requiredFields = ['title', 'content', 'author', 'publishDate', 'id'];
+    //    for (let i = 0; i < requiredFields.length; i++) {
+    //        const field = requiredFields[i];
+    //        if (!(field in req.body)) {
+    //            const message = `Missing \`${field}\` in request body`
+    //            console.error(message);
+    //            return res.status(400).send(message);
+    //        }
+    //    }
+    //    if (req.params.id !== req.body.id) {
+    //        const message = (
+    //            `Request path id (${req.params.id}) and request body id `
+    //            `(${req.body.id}) must match`);
+    //        console.error(message);
+    //        return res.status(400).send(message);
+    //    }
+    //    console.log(`Updating blog item \`${req.params.id}\``);
+    //    const updatedItem = BlogPostss.update({
+    //        id: req.params.id,
+    //        title: req.body.title,
+    //        content: req.body.content,
+    //        author: req.body.author,
+    //        publishDate: req.body.publishDate
+    //    });
+    //    res.status(204).json(updatedItem);
+    //})
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        res.status(400).json({
+            error: 'Request path id and request body id values must match'
+        });
     }
-    if (req.params.id !== req.body.id) {
-        const message = (
-            `Request path id (${req.params.id}) and request body id `
-            `(${req.body.id}) must match`);
-        console.error(message);
-        return res.status(400).send(message);
-    }
-    console.log(`Updating blog item \`${req.params.id}\``);
-    const updatedItem = BlogPostss.update({
-        id: req.params.id,
-        title: req.body.title,
-        content: req.body.content,
-        author: req.body.author,
-        publishDate: req.body.publishDate
-    });
-    res.status(204).json(updatedItem);
-})
 
+    const updated = {};
+    const updateableFields = ['title', 'content', 'author'];
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            updated[field] = req.body[field];
+        }
+    });
+
+    BlogPosts
+        .findByIdAndUpdate(req.params.id, {
+            $set: updated
+        }, {
+            new: true
+        })
+        .exec()
+        .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
+        .catch(err => res.status(500).json({
+            message: 'Internal server errir'
+        }));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoos.Promise = global.Promise;
 
+const {
+    PORT, DATABASE_URL
+} = require('./config');
+const {
+    BlogPosts
+} = require('./models');
 const app = express();
 
 const blogPostRouter = require('./blogPostRouter');
@@ -16,49 +24,46 @@ app.use('/blogPostRouter', blogPostRouter);
 
 let server;
 
-function runServer() {
-    const port = process.env.PORT || 8080;
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+
     return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            console.log(`Your app is listening on port ${port}`);
-            resolve(server);
-        }).on('error', err => {
-            reject(err)
-        });
-    });
-}
-//
-//// like `runServer`, this function also needs to return a promise.
-//// `server.close` does not return a promise on its own, so we manually
-//// create one.
-function closeServer() {
-    return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
+        mongoose.connect(databaseUrl, err => {
             if (err) {
-                reject(err);
-                // so we don't also call `resolve()`
-                return;
+                return reject(err);
             }
-            resolve();
+            server = app.listen(port, () => {
+                    console.log(`Your app is listening on port ${port}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
         });
     });
 }
-//
-//// if server.js is called directly (aka, with `node server.js`), this block
-//// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
-//if (require.main === module) {
-//    runServer().catch(err => console.error(err));
-//};
-//
 
-
-
-if (!module.parent) {
-    app.listen(process.env.PORT || 8080, () => {
-        console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
     });
 }
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
 
 module.exports = {
     app, runServer, closeServer
